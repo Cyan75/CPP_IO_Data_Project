e733723c6a2b42acc5e10cf945639545e7210389
Cyan75 <incanto075@gmail.com>
2021-02-16 23:56:35 +0900

    added comments, MBTI enum algorithm needed

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 /* 
 taking strings/ints/enum for an element: name, age, city, mbti
 prevent stack overflow e.g. age over 99999999999999
-action: sort by name, age, city, mbti
+action: sort by name, age, mbti, location
 return tabular csv
+
+make header(*.h) and class(*.cpp) files for each class
+?read data separated w/ comma ","(44 in ASCII) ; e.g.  Susan,23,ESTJ,Chicago,US
 */
 #include <iostream>
 class PersonalInfo

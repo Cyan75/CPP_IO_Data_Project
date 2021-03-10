c9121181356a526771b8ab546acc4a573b99f1d8
Cyan75 <incanto075@gmail.com>
2021-03-10 23:55:59 +0900

    changed nameSorter.cpp and mbtiCombination.cpp to create txt result files

--- mbtiCombination.cpp
+++ mbtiCombination.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 //generic form for MBTI types : should generate every single type
 /*use dynamic allocation to prevent using all 16 'slots' for 16 types
 repeate the sequence in an order
@@ -10,19 +11,28 @@ int main(void)
 {
     std::vector<char> entp = {'E', 'N', 'T', 'P'};
     std::vector<char> isfj = {'I', 'S', 'F', 'J'};
+    std::ofstream mbti("MBTICombination.txt");
     for (short it = 0; it < 16; ++it)
-  {
+    {
         std::string *pStr = new std::string;
         for (short itMode = 0; itMode < 4; ++itMode)
         {
             if (((it >> itMode) & 1) == 1)
+            {
                 *pStr = *pStr + entp[itMode];
+            }
             else
+            {
                 *pStr = *pStr + isfj[itMode];
+            }
         }
-        std::cout << *pStr << ", ";
-
+        mbti << *pStr;
+        if (it != 15)
+        {
+            mbti << std::endl;
+        }
         delete pStr;
     }
+    mbti.close();
     return 0;
 }

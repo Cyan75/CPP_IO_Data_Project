68dd4e5fe1e02672085cb2679106ff94cab0dd6f
Cyan75 <incanto075@gmail.com>
2021-03-15 23:59:12 +0900

    separated compareStringSize and compareStringContents

--- DuplicateElementChecker.cpp
+++ DuplicateElementChecker.cpp
@@ -2,5 +2,16 @@
 #include<string>
 #include<fstream>
 
+int main(void)
+{
+    std::string s="hello";
+    ;
+    for (std::string::iterator it =s.begin(); it != s.end(); ++it)
+    {
+        std::cout<<*it<<" ";
+    }
+    
+    return 0;
+}
 
 
--- StringSorter.cpp
+++ StringSorter.cpp
@@ -3,76 +3,23 @@
 #include <sstream>
 #include <ostream>
 #include <string>
+#include <vector>
 
 class StringSorter
 {
-private:
-    std::string fileName;
-    void compareSizeTwoStrings(std::string primary, std::string secondary) //first
-    {
-        std::string::iterator itP = primary.begin();
-        std::string::iterator itS = secondary.begin();
-        if (primary.size() == secondary.size())
-        {
-            /* 
-            Joe          J       o       e
-            Joy          J       o       y
-            */
-            for (short i = 0; i < secondary.size(); ++i)
-            {
-                if (*itP != *itS)
-                {
-                    short numP = static_cast<short>(*itP);
-                    short numS = static_cast<short>(*itS);
-                    if (numP < numS)
-                    {
-                        /* LET IT BE */
-                    }
-                    else
-                    {
-                        /* INTERCHANGE 
-                    interchange(*/
-                    }
-                }
-                //do not need to consider uppercase, lowercase comparison
-                //only the initials are capitalised
-                //should be modified when there is a name with a uppercase in the middle
-                else
-                {
-                }
-            }
-        }
-
-        else //primary.size() != secondary.size()
-        {
-            /* 
-            Joe          J       o       e
-            Joy          J       o       y
-            Joya         J       o       y       a 
-            */
-            short min = std::min(primary.size(), secondary.size());
-            for (short i = 0; i < min; ++i)
-            {
-            }
-        }
-    }
-    //
-    void compareTwoLetter(char firstLetter, char secondLetter)
-    {
-    }
-    void interchange(std::string primary, std::string secondary)
-    {
-        /* code */
-    }
-
 public:
+    /* sort: compare every possible pair of any two lines */
     StringSorter(std::string inputFileName)
     {
         fileName = inputFileName;
-        std::ifstream listOfString(fileName);
-        if (listOfString.is_open())
+        std::ifstream inputFile(fileName);
+        if (inputFile.is_open())
         {
             std::string line;
+            while (std::getline(inputFile, line))
+            {
+                unsorted.push_back(line);
+            }
         }
         else
         {
@@ -92,5 +39,70 @@ public:
             }
         }
         return numLines;
+    }
+
+private:
+    std::string fileName;
+    std::vector<std::string> unsorted;
+    std::vector<std::string>::iterator itU = unsorted.begin();
+    void CompareStringSize(std::string primary, std::string secondary) //first
+    {
+        std::string::iterator itP = primary.begin();
+        std::string::iterator itS = secondary.begin();
+        if (primary.size() == secondary.size())
+        {
+            /* 
+            Joe          J       o       e
+            Joy          J       o       y
+            */
+            for (short i = 0; i < secondary.size(); ++i)
+            {
+                compareStringContents(itP,itS);
+            }
+        }
+        else //primary.size() != secondary.size()
+        {
+            /* 
+            Joe          J       o       e
+            Joy          J       o       y
+            Joya         J       o       y       a 
+            */
+            short min = std::min(primary.size(), secondary.size());
+            for (short i = 0; i < min; ++i)
+            {
+                compareStringContents(itP,itS);
+            }
+            //longer string to be placed later
+        }
+    }
+    void compareStringContents(std::string::iterator it1, std::string::iterator it2)
+    {
+        if (*it1 != *it2)
+        {
+            short numP = static_cast<short>(*it1);
+            short numS = static_cast<short>(*it2);
+            if (numP > numS)
+            {
+                /* INTERCHANGE 
+                    interchange()
+                    */
+            }
+            else
+            {
+                /* LET IT BE */
+            }
+        }
+        //do not need to consider uppercase, lowercase comparison
+        //only the initials are capitalised
+        //should be modified when there is a name with a uppercase in the middle
+        else
+        {
+            it1 = it1++;
+            it2 = it2++;
+        }
+    }
+    void interchange(std::string primary, std::string secondary)
+    {
+        /* code */
     }
 };

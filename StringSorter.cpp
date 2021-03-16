32724ce845a42f7b2aec95af2916c5b0641a7db8
Cyan75 <incanto075@gmail.com>
2021-03-16 23:54:12 +0900

    tidied up the determinating functions for sort function

--- StringSorter.cpp
+++ StringSorter.cpp
@@ -8,23 +8,27 @@
 class StringSorter
 {
 public:
-    /* sort: compare every possible pair of any two lines */
-    StringSorter(std::string inputFileName)
+    StringSorter(std::string inputFileName, std::string outputFileName)
     {
-        fileName = inputFileName;
-        std::ifstream inputFile(fileName);
+        *iFileName = inputFileName;
+        *oFileName = outputFileName;
+        std::vector<std::string> *unsorted = new std::vector<std::string>;
+        //std::vector<std::string>::iterator itU = unsorted->begin();
+        std::ifstream inputFile(*iFileName);
         if (inputFile.is_open())
         {
             std::string line;
             while (std::getline(inputFile, line))
             {
-                unsorted.push_back(line);
+                unsorted->push_back(line);
             }
         }
         else
         {
             std::cout << "unable to open the text file" << std::endl;
         }
+        sort(*unsorted, *oFileName);
+        delete unsorted;
     }
     short getFileLines(std::string fileName)
     {
@@ -42,67 +46,55 @@ public:
     }
 
 private:
-    std::string fileName;
-    std::vector<std::string> unsorted;
-    std::vector<std::string>::iterator itU = unsorted.begin();
-    void CompareStringSize(std::string primary, std::string secondary) //first
+    std::string *iFileName = new std::string;
+    std::string *oFileName = new std::string;
+    bool ifTwoStringShoulBeInterchanged(std::string primary, std::string secondary)
     {
         std::string::iterator itP = primary.begin();
         std::string::iterator itS = secondary.begin();
-        if (primary.size() == secondary.size())
+        /* 
+            Joe          J 74       o 111      e 101     '' 0
+            Joy          J 74       o 111      y 121     '' 0
+            Joya         J 74       o 111      y 121      a 97
+            Fio          F 70       i 105      o 111     '' 0
+        */
+        while (*itP == *itS)
         {
-            /* 
-            Joe          J       o       e
-            Joy          J       o       y
-            */
-            for (short i = 0; i < secondary.size(); ++i)
-            {
-                compareStringContents(itP,itS);
-            }
+            itP++;
+            itS++;
+            // duplication exceptions?
         }
-        else //primary.size() != secondary.size()
+        //*itP != *itS
+        if (ifTwoCharsAreInOrder(itP, itS))
         {
-            /* 
-            Joe          J       o       e
-            Joy          J       o       y
-            Joya         J       o       y       a 
-            */
-            short min = std::min(primary.size(), secondary.size());
-            for (short i = 0; i < min; ++i)
-            {
-                compareStringContents(itP,itS);
-            }
-            //longer string to be placed later
+            return true;
         }
-    }
-    void compareStringContents(std::string::iterator it1, std::string::iterator it2)
-    {
-        if (*it1 != *it2)
-        {
-            short numP = static_cast<short>(*it1);
-            short numS = static_cast<short>(*it2);
-            if (numP > numS)
-            {
-                /* INTERCHANGE 
-                    interchange()
-                    */
-            }
-            else
-            {
-                /* LET IT BE */
-            }
-        }
-        //do not need to consider uppercase, lowercase comparison
-        //only the initials are capitalised
-        //should be modified when there is a name with a uppercase in the middle
         else
         {
-            it1 = it1++;
-            it2 = it2++;
+            return false;
         }
     }
-    void interchange(std::string primary, std::string secondary)
+    bool ifTwoCharsAreInOrder(std::string::iterator it1, std::string::iterator it2) //special characters? ¿, ú, á, ô ... s
+    {
+        short numP = static_cast<short>(*it1);
+        short numS = static_cast<short>(*it2);
+        if (numP > numS)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void sort(std::vector<std::string> pListOfStrings, std::string oFileName)
     {
         /* code */
+        
+        std::ofstream outputFile;
+        if (outputFile.is_open())
+        {
+            
+        }
     }
 };
